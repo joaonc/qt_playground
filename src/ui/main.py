7efffe7e0da8027d91c8e043dd1525a8511e9dea
Joao Coelho <joao@xealenergy.com>
2024-04-30 08:39:13 -0500

    load from file

--- src/ui/main.py
+++ src/ui/main.py
@@ -1,15 +1,22 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+
+from src import ASSETS_DIR
 
 
 def main():
     # Need one (and only one) `QApplication` instance per application.
-    # Pass in `sys.argv` to allow command line arguments for the app.
+    # `sys.argv` to allow command line arguments for the app.
     # If command line arguments re not used, `QApplication([])` works too.
     app = QApplication(sys.argv)
-    window = QWidget()  # Create a Qt widget, which will be our window.
-    window.show()  # Windows are hidden by default.
+    file = QFile(ASSETS_DIR / 'ui/main.ui')
+    file.open(QFile.OpenModeFlag.ReadOnly)
+    loader = QUiLoader()
+    dialog = loader.load(file)
+    dialog.show()
 
     # Start the event loop.
     sys.exit(app.exec())
