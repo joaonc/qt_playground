fad6aed34dc447ef54857037b39def1bec30f764
Joao Coelho <joaonc@hotmail.com>
2024-06-16 18:05:35 -0500

    resource files

---
+++ src/ui/forms/resources_rc.py
@@ -0,0 +1,369 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.7.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x14\x8f\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22UTF\
+-8\x22 standalone=\x22\
+no\x22?>\x0a<!-- Creat\
+ed with Inkscape\
+ (http://www.ink\
+scape.org/) -->\x0a\
+\x0a<svg\x0a   xmlns:d\
+c=\x22http://purl.o\
+rg/dc/elements/1\
+.1/\x22\x0a   xmlns:cc\
+=\x22http://creativ\
+ecommons.org/ns#\
+\x22\x0a   xmlns:rdf=\x22\
+http://www.w3.or\
+g/1999/02/22-rdf\
+-syntax-ns#\x22\x0a   \
+xmlns:svg=\x22http:\
+//www.w3.org/200\
+0/svg\x22\x0a   xmlns=\
+\x22http://www.w3.o\
+rg/2000/svg\x22\x0a   \
+xmlns:sodipodi=\x22\
+http://sodipodi.\
+sourceforge.net/\
+DTD/sodipodi-0.d\
+td\x22\x0a   xmlns:ink\
+scape=\x22http://ww\
+w.inkscape.org/n\
+amespaces/inksca\
+pe\x22\x0a   width=\x2230\
+\x22\x0a   height=\x2230\x22\
+\x0a   id=\x22svg2\x22\x0a  \
+ sodipodi:versio\
+n=\x220.32\x22\x0a   inks\
+cape:version=\x220.\
+48.4 r9939\x22\x0a   s\
+odipodi:docname=\
+\x22school.svg\x22\x0a   \
+inkscape:output_\
+extension=\x22org.i\
+nkscape.output.s\
+vg.inkscape\x22\x0a   \
+version=\x221.1\x22>\x0a \
+ <defs\x0a     id=\x22\
+defs4\x22 />\x0a  <sod\
+ipodi:namedview\x0a\
+     id=\x22base\x22\x0a \
+    pagecolor=\x22#\
+ffffff\x22\x0a     bor\
+dercolor=\x22#66666\
+6\x22\x0a     borderop\
+acity=\x221.0\x22\x0a    \
+ inkscape:pageop\
+acity=\x220.0\x22\x0a    \
+ inkscape:pagesh\
+adow=\x222\x22\x0a     in\
+kscape:zoom=\x2211.\
+6\x22\x0a     inkscape\
+:cx=\x2215.344828\x22\x0a\
+     inkscape:cy\
+=\x2214.482759\x22\x0a   \
+  inkscape:docum\
+ent-units=\x22px\x22\x0a \
+    inkscape:cur\
+rent-layer=\x22laye\
+r1\x22\x0a     inkscap\
+e:window-width=\x22\
+698\x22\x0a     inksca\
+pe:window-height\
+=\x221029\x22\x0a     ink\
+scape:window-x=\x22\
+0\x22\x0a     inkscape\
+:window-y=\x2219\x22\x0a \
+    showgrid=\x22fa\
+lse\x22\x0a     inksca\
+pe:window-maximi\
+zed=\x220\x22 />\x0a  <me\
+tadata\x0a     id=\x22\
+metadata7\x22>\x0a    \
+<rdf:RDF>\x0a      \
+<cc:Work\x0a       \
+  rdf:about=\x22\x22>\x0a\
+        <dc:form\
+at>image/svg+xml\
+</dc:format>\x0a   \
+     <dc:type\x0a  \
+         rdf:res\
+ource=\x22http://pu\
+rl.org/dc/dcmity\
+pe/StillImage\x22 /\
+>\x0a        <dc:ti\
+tle></dc:title>\x0a\
+      </cc:Work>\
+\x0a    </rdf:RDF>\x0a\
+  </metadata>\x0a  \
+<g\x0a     inkscape\
+:label=\x22Capa 1\x22\x0a\
+     inkscape:gr\
+oupmode=\x22layer\x22\x0a\
+     id=\x22layer1\x22\
+\x0a     transform=\
+\x22translate(0,-10\
+22.3622)\x22>\x0a    <\
+path\x0a       sodi\
+podi:type=\x22arc\x22\x0a\
+       style=\x22co\
+lor:#000000;fill\
+:#1a1a1a;stroke:\
+#d4d4d4;stroke-w\
+idth:1.02777779;\
+stroke-opacity:1\
+;marker:none;vis\
+ibility:visible;\
+display:inline;o\
+verflow:visible;\
+enable-backgroun\
+d:accumulate\x22\x0a  \
+     id=\x22path166\
+17-6-6\x22\x0a       s\
+odipodi:cx=\x2215.1\
+6226\x22\x0a       sod\
+ipodi:cy=\x2216.568\
+51\x22\x0a       sodip\
+odi:rx=\x2214.00841\
+3\x22\x0a       sodipo\
+di:ry=\x2214.008413\
+\x22\x0a       d=\x22m 29\
+.170673,16.56851\
+ a 14.008413,14.\
+008413 0 1 1 -28\
+.0168263,0 14.00\
+8413,14.008413 0\
+ 1 1 28.0168263,\
+0 z\x22\x0a       tran\
+sform=\x22matrix(0.\
+97297297,0,0,0.9\
+7297297,330.5079\
+5,953.63241)\x22 />\
+\x0a    <path\x0a     \
+  style=\x22fill:#1\
+a1a1a;stroke:#d4\
+d4d4;stroke-widt\
+h:1px;stroke-lin\
+ecap:butt;stroke\
+-linejoin:miter;\
+stroke-opacity:1\
+\x22\x0a       d=\x22m 34\
+5.29648,957.8901\
+1 0,11.863 0,0\x22\x0a\
+       id=\x22path1\
+6623-6-2\x22\x0a      \
+ inkscape:connec\
+tor-curvature=\x220\
+\x22\x0a       inkscap\
+e:transform-cent\
+er-y=\x22-5.9314904\
+\x22 />\x0a    <path\x0a \
+      style=\x22fil\
+l:#1a1a1a;stroke\
+:#d4d4d4;stroke-\
+width:1px;stroke\
+-linecap:butt;st\
+roke-linejoin:mi\
+ter;stroke-opaci\
+ty:1\x22\x0a       d=\x22\
+m 357.15947,969.\
+75311 -1.70877,0\
+ 0,0\x22\x0a       id=\
+\x22path16623-4-2-0\
+\x22\x0a       inkscap\
+e:connector-curv\
+ature=\x220\x22\x0a      \
+ inkscape:transf\
+orm-center-x=\x22-0\
+.854382\x22 />\x0a    \
+<path\x0a       sty\
+le=\x22fill:#1a1a1a\
+;stroke:#d4d4d4;\
+stroke-width:0.9\
+9999994px;stroke\
+-linecap:butt;st\
+roke-linejoin:mi\
+ter;stroke-opaci\
+ty:1\x22\x0a       d=\x22\
+m 345.29648,981.\
+61611 0,-2.0181 \
+0,0\x22\x0a       id=\x22\
+path16623-4-3-9-\
+0\x22\x0a       inksca\
+pe:connector-cur\
+vature=\x220\x22\x0a     \
+  inkscape:trans\
+form-center-y=\x221\
+.0091015\x22 />\x0a   \
+ <path\x0a       st\
+yle=\x22fill:#1a1a1\
+a;stroke:#d4d4d4\
+;stroke-width:1p\
+x;stroke-linecap\
+:butt;stroke-lin\
+ejoin:miter;stro\
+ke-opacity:1\x22\x0a  \
+     d=\x22m 333.43\
+348,969.75311 11\
+.863,0 0,0\x22\x0a    \
+   id=\x22path16623\
+-4-3-4-1-0\x22\x0a    \
+   inkscape:conn\
+ector-curvature=\
+\x220\x22\x0a       inksc\
+ape:transform-ce\
+nter-x=\x225.9315\x22 \
+/>\x0a    <path\x0a   \
+    inkscape:con\
+nector-curvature\
+=\x220\x22\x0a       sodi\
+podi:nodetypes=\x22\
+cccccccccccccc\x22\x0a\
+       id=\x22path9\
+518-0-7\x22\x0a       \
+d=\x22m 14.999994,1\
+025.9915 -11.242\
+2196,9.9235 2.24\
+84301,0 0,12.817\
+7 6.7453475,0 0,\
+-7.5644 4.651059\
+,0 1.3e-5,7.5644\
+ 6.591159,0 0,-1\
+2.8177 2.248444,\
+0 -11.242237,-9.\
+9235 0,0 0,0 z\x22\x0a\
+       style=\x22fo\
+nt-size:12px;fil\
+l:none;stroke:no\
+ne\x22 />\x0a    <path\
+\x0a       inkscape\
+:connector-curva\
+ture=\x220\x22\x0a       \
+style=\x22fill:#1a1\
+a1a;fill-opacity\
+:1;fill-rule:eve\
+nodd;stroke:none\
+\x22\x0a       d=\x22m 8.\
+1568966,1028.524\
+6 c -1.229871,0 \
+-2.2500001,0.988\
+9 -2.2500001,2.2\
+188 0,1.2298 1.0\
+201291,2.2187 2.\
+2500001,2.2187 1\
+.2298709,0 2.218\
+7504,-0.9889 2.2\
+187504,-2.2187 0\
+,-1.2299 -0.9888\
+795,-2.2188 -2.2\
+187504,-2.2188 z\
+ m 17.0937494,3.\
+4688 c -0.711535\
+,0.069 -1.3831,0\
+.4808 -1.75,1.15\
+62 -0.58704,1.08\
+07 -0.174475,2.4\
+13 0.90625,3 1.0\
+80725,0.587 2.41\
+296,0.2057 3,-0.\
+875 0.58704,-1.0\
+807 0.205725,-2.\
+4442 -0.875,-3.0\
+312 -0.405272,-0\
+.2202 -0.854329,\
+-0.2914 -1.28125\
+,-0.25 z m -18.9\
+062494,0.3437 c \
+-0.8199201,-0.17\
+06 -3.0066561,2.\
+6958 -4.0000001,\
+4.0938 l -0.0625\
+,0 c -0.488847,-\
+0.096 -0.966308,\
+0.2299 -1.0625,0\
+.7187 -0.09619,0\
+.4889 0.198653,0\
+.9663 0.6875,1.0\
+625 l 3.5625,0.6\
+875 c -0.413313,\
+0.5777 -1.020325\
+,1.4367 -1.65625\
+,2.25 -0.586564,\
+0.7502 0.196219,\
+1.3925 1,0.625 1\
+.069246,-1.021 1\
+.9005511,-1.9603\
+ 2.3750001,-2.53\
+12 l 7.5625004,1\
+.5 -2.46875,5.12\
+5 5.499999,0 -2.\
+406249,-5 6.9062\
+49,1.3437 c 0.15\
+2593,0.7586 0.37\
+9564,1.9339 0.81\
+25,3.2813 0.3399\
+72,1.058 1.34487\
+2,0.8454 1.1875,\
+-0.094 -0.184261\
+,-1.0997 -0.2940\
+01,-2.2139 -0.37\
+5,-2.875 l 3.812\
+5,0.75 c 0.48884\
+7,0.096 0.966308\
+,-0.1987 1.0625,\
+-0.6875 0.09619,\
+-0.4889 -0.22990\
+3,-0.9664 -0.718\
+75,-1.0625 l -0.\
+3125,-0.063 c -0\
+.238338,-1.8185 \
+-0.736566,-4.779\
+6 -1.46875,-5 -0\
+.8142,-0.2452 -2\
+.652346,2.5063 -\
+3.59375,4 l -15.\
+0937494,-2.968 c\
+ -0.117488,-1.77\
+49 -0.426181,-4.\
+9849 -1.25,-5.15\
+63 z\x22\x0a       id=\
+\x22path2385-6\x22 />\x0a\
+  </g>\x0a</svg>\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x06\
+\x07\x03}\xc3\
+\x00i\
+\x00m\x00a\x00g\x00e\x00s\
+\x00\x1a\
+\x0a\xf1\x8e\xa7\
+\x00p\
+\x00l\x00a\x00y\x00g\x00r\x00o\x00u\x00n\x00d\x00_\x00i\x00c\x00o\x00n\x00_\x001\
+\x008\x007\x008\x006\x000\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x90#G>\x0d\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
